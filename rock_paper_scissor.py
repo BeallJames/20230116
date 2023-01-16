27594ae47744b08eeb751e98726514c6410fa99c
BeallJames <bealljamesp@proton.me>
2023-01-16 11:02:59 -0800

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,4 +2,15 @@ import random
 
 def play():
     user = input("'r' for rock, 'p' for paper, 's' for scissors")
-    computer = 
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'tie'
+
+def is_win(player, opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r')
+    return True
+
+def is_lose(player, opponent):
+    if (player == 'r' and opponent == 'p') or (player == 's' and opponent == 'r') or (player == 'p' and opponent == 's')
+    return True
